c737365f864190b4fa3a912822510f64d75df41a
andreabeverini <117717829+andreabeverini@users.noreply.github.com>
2022-12-03 10:23:44 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -4,7 +4,8 @@ bool numprimi (int a) {
   bool risultato=1;
   for (int i=2; i<a; i++) {
     if (a%i==0) {
-      return risultato; }
+      return risultato; 
+    }
   }
   risultato=0;
   return risultato;
